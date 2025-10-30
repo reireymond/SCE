f038dcfe212a8569958fe20b1c03106541873236
gabriel <bielbieloliveirag8@gmail.com>
2025-10-30 14:15:07 -0300

    consertos

--- controller/login/login_controller.h
+++ controller/login/login_controller.h
@@ -3,8 +3,7 @@
 
 #include "model/sistema.h" // Para acessar a estrutura Sistema
 
-// Função principal que gerencia o fluxo de login ou primeiro cadastro
 // Retorna 1 se o login for bem-sucedido, 0 caso contrário (ou se o usuário sair)
 int realizarLoginOperador(Sistema *sistema);
 
-#endif // LOGIN_CONTROLLER_H
+#endif 
