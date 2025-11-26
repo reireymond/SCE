82016410187b9d5cdf39cb807f588a7bac9ccf2a
Kaua Teixeira <68856326+reireymond@users.noreply.github.com>
2025-11-26 19:28:09 -0300

    Refactor data transfer function to use parameters

--- controller/armazenamento/armazenamento_controller.c
+++ controller/armazenamento/armazenamento_controller.c
@@ -4,9 +4,6 @@
 #include <string.h>
 #include "model/config_armazenamento/config_armazenamento.h"
 #include "controller/main/main_controller.h"
-#include "utils/utils.h"
-#include "utils/validation.h"
-// Includes para as funcoes de salvar/carregar
 #include "model/produtora/produtora_model.h"
 #include "model/cliente/cliente_model.h"
 #include "model/equipe_interna/equipe_interna_model.h"
@@ -16,47 +13,15 @@
 #include "model/evento/evento_model.h"
 #include "model/transacao/transacao_model.h"
 
-void transferirDadosDeArmazenamento(Sistema *sistema) {
-    
-    int op1, op2;
-    TipoArmazenamento origem, destino;
-
-    limpar_tela();
-    printf("=== CONVERTER ARQUIVOS ===\n");
-    printf("Origem: [1] Binario [2] Texto: ");
-    scanf("%d", &op1);
-    if(op1 == 1) origem = ARQUIVO_BINARIO; else origem = ARQUIVO_TEXTO;
-
-    printf("Destino: [1] Binario [2] Texto: ");
-    scanf("%d", &op2);
-    if(op2 == 1) destino = ARQUIVO_BINARIO; else destino = ARQUIVO_TEXTO;
-
-    if (origem == destino) { printf("Mesmo formato, nada a fazer.\n"); return; }
-
-    printf("\nTem certeza que deseja converter de [%s] para [%s]? (s/n): ", 
-           (origem == ARQUIVO_BINARIO ? "Binario" : "Texto"),
-           (destino == ARQUIVO_BINARIO ? "Binario" : "Texto"));
-    
-    char conf; 
-    scanf(" %c", &conf); 
-    limpar_buffer();
-
-    if (conf != 's' && conf != 'S') {
-        printf("Transferencia cancelada.\n");
-        return;
-    }
-    
+void converterDadosController(Sistema *sistema, TipoArmazenamento origem, TipoArmazenamento destino) {
     Sistema temp;
     memset(&temp, 0, sizeof(Sistema));
 
-    printf("Lendo origem...\n");
     definirModoDeArmazenamento(&temp, origem);
     carregarTodosOsDadosController(&temp);
 
-    printf("Salvando destino...\n");
     definirModoDeArmazenamento(&temp, destino);
     
-    // Salva tudo no formato novo
     salvarProdutora(&temp);
     salvarClientes(&temp);
     salvarEquipeInterna(&temp);
@@ -66,8 +31,7 @@ void transferirDadosDeArmazenamento(Sistema *sistema) {
     salvarEventos(&temp);
     salvarTransacoes(&temp);
 
-
     liberarTodaMemoriaController(&temp);
    
-    printf(" Conversao concluida com sucesso! Novos arquivos criados.\n"); 
+    printf("\nProcesso interno finalizado! Arquivos convertidos.\n"); 
 }
