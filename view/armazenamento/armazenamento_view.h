42bf1c9ee7f17cea7cb3c4ef925ea2c8c5c1e214
Kaua Teixeira <68856326+reireymond@users.noreply.github.com>
2025-11-26 19:26:13 -0300

    Add header for armazenamento_view with conversion menu

---
+++ view/armazenamento/armazenamento_view.h
@@ -0,0 +1,9 @@
+#ifndef ARMAZENAMENTO_VIEW_H
+#define ARMAZENAMENTO_VIEW_H
+
+#include "model/sistema.h"
+
+// Menu que pergunta os tipos de arquivo e chama a conversao
+void menuConverterArquivosView(Sistema *sistema);
+
+#endif
